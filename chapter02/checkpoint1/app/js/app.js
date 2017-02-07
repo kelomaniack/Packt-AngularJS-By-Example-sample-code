9d4392f9ec99f71e3fac613bfb1a6e0104cf3bca
e_konstantinidis <e.konstantinidis@app-smart.de>
2017-02-07 13:13:36 +0100

    Finished Chapter 2: Building Our First App- 7 Minute Workout

--- chapter02/checkpoint1/app/js/app.js
+++ chapter02/checkpoint1/app/js/app.js
@@ -1,5 +1,28 @@
 'use strict';
 
-angular.module('app', []);
+angular.module('app', ['ngRoute', '7minWorkout'])
+    .config(function ($routeProvider, $sceDelegateProvider) {
+        $routeProvider.when('/start', {
+            templateUrl: 'partials/start.html'
+        });
+        $routeProvider.when('/workout', {
+            templateUrl: 'partials/workout.html',
+            controller: 'WorkoutController'
+        });
+        $routeProvider.when('/finish', {
+            templateUrl: 'partials/finish.html'
+        });
+        $routeProvider.otherwise({
+            redirectTo: '/start'
+        });
+        // use the HTML5 History API
+        //$locationProvider.html5Mode(true);
+
+        // Allow same origin resource loads and from youtube
+        $sceDelegateProvider.resourceUrlWhitelist([
+            'self',
+            'http://*.youtube.com/**']);
+
+    });
 
 angular.module('7minWorkout', []);
